1beba617a5c20f48b2cec37a6c8d29e2d15eb1cb
Evan Berkowitz <berkowitz2@llnl.gov>
2016-09-29 21:44:37 -0700

    Update surface settings.

--- machines/surface.sh
+++ machines/surface.sh
@@ -1,35 +1,27 @@
-BASE=/g/g14/berkowit/USQCD/surface
-INSTALL=/p/lscratche/berkowit/USQCD/surface
-GPUS=false
+BASE=/p/lscratche/berkowit/USQCD_TEST/surface
+GPUS=1
 
 # load dotkit
 . /usr/local/tools/dotkit/init.sh
 
 module purge
-use hdf5-gnu-parallel-mvapich2-1.8.16
-use mvapich2-gnu-2.2
+use hdf5-gnu-parallel-mvapich2-1.8.16 > /dev/null
+use mvapich2-gnu-2.2                  > /dev/null
 module load cudatoolkit/7.5
 module load gnu/4.9.2
 module load fftw/3.2
 
-SM=sm_35  # Kepler GK110 at Keeneland
-PK_CUDA_HOME=${CUDA_INSTALL_PATH}    # At LLNL Loading the module sets this. Otherwise do by hand
-PK_GPU_ARCH=${SM}                    # At LLNL Edge cluster we have Fermi architecture
-
 HOST=x86_64-linux-gnu
+SM=sm_35  # Kepler GK110 at Keeneland
 
-HDF5=$HDF5
-FFTW=${FFTW_LIBS%/*}
+INSTALL[hdf5]=$HDF5
+INSTALL[fftw]=${FFTW_LIBS%/*}
+
+STACK="qmp libxml2 qdpxx quda chroma"
+
 CC=mpicc
 CXX=mpicxx
 
-QMP_BRANCH=master
-QDPXX_BRANCH=master
-CHROMA_BRANCH=master
-LIBXML2_BRANCH=v2.9.4
-
-OMPFLAGS="-fopenmp"
-OMPENABLE="--enable-openmp"
-CFLAGS=${OMPFLAGS}" -g -O2 -std=c99 -march=core2"
-CXXFLAGS=${OMPFLAGS}" -g -O2 -std=c++11 -march=core2"
-MAKE_JOBS=10
+C_FLAGS[DEFAULT]="-fopenmp -g -O2 -std=c99 -march=core2"
+CXX_FLAGS[DEFAULT]="-fopenmp -g -O2 -std=c++11 -march=core2"
+CONFIG_FLAGS[chroma]+=" --enable-sse-scalarsite-bicgstab-kernels --enable-sse2 --enable-sse3"

74733521dd9da48df3cbe80d9f4641df11f76225
Evan Berkowitz <git@evanberkowitz.com>
2016-09-29 17:30:02 -0700

    Commit usqcd.sh
    Meant to git add this in the previous commit.

--- usqcd.sh
+++ usqcd.sh
@@ -2,214 +2,188 @@
 # Your preferred bash must have associative arrays, or LOTS of things will fail.
 # Practically speaking, this means you need bash version 4 or later.
 
-# USQCD Install Script, by Evan Berkowitz
-# for my Mac, as of 2016-07-01
-# 2.7 GHz Intel Core i7
-# NVIDIA GeForce GT 650M 1024MB
-# OS X 10.9.5
+MACHINE=$1
+LIBRARY=$2
+ACTION=$3
 
-if [[ $# -ne 3 ]]; then
-    echo "Exactly 3 requirements: settings_file.sh action library"
-	echo "action can be one of:"
-	echo "    fetch    get the library from the repo"
-	echo "    make     configure and build"
-	echo "    install  put into the final destination"
-	echo "    complete fetch, make, and then install"
-	echo "    clean    remove the relevant source, build, log, and install"
-	echo "library can be one of:"
-	echo "    qmp"
-    echo "    libxml2"
-	echo "    qdpxx"
-	echo "    chroma"
-	echo "    all      qmp, libxml2, qdpxx, and then chroma."
-	exit
+this_script=$0
+script_folder=${this_script%/*}
+source ${script_folder}/machines/$MACHINE.sh    # Read the user options, as some are needed for
+source ${script_folder}/machines/default.sh     # the default settings
+source ${script_folder}/machines/$MACHINE.sh    # after which, the user can over-ride the defaults
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+function UNQUOTE {
+    s="$*"
+    while [[ "$s" != "$(eval echo \"$s\")" ]]; do
+        s="$(eval echo \"$s\")"
+    done
+    echo "$s"
+}
+
+function PROMPT_USER {
+    prompt="$1"
+    if [[ 2 == $# ]]; then
+        prompt+=" [$2]"
+    fi
+    read -p "$prompt " response
+    if [[ -z "$response" ]]; then
+        response="$2";
+    fi
+    echo $response
+}
+
+if [[ "all" == "$LIBRARY" ]]; then
+    for library in $(UNQUOTE $STACK); do
+        $this_script $MACHINE $library $ACTION
+        if [[ $status -ne 0 ]]; then
+            echo ... failed; exit 1;
+        fi
+    done
+    exit
 fi
 
-DEFAULT_BRANCH=master
-BUILD_FLAG="none"
+needed=""
+for library in $(UNQUOTE $STACK); do
+    if [[ "$LIBRARY" == "$library" ]]; then
+        needed="yes"
+    fi
+done
+if [[ -z "$needed" ]]; then
+    echo "$LIBRARY is not needed according to your STACK: $(UNQUOTE $STACK)"
+    exit
+fi
 
-settings=$1
-source $settings
-library=$2
-action=$3
 
+for folder in SOURCE BUILD INSTALL LOG; do
+    mkdir -p $(UNQUOTE "${DIR[$folder]}")
+done
+for folder in BUILD INSTALL LOG; do
+    mkdir -p $(UNQUOTE "\${$folder[$LIBRARY]}")
+done
 
-SOURCE=$BASE/source
-BUILD=$BASE/build
-LOG=$BASE/log
-CONFIG_FLAGS="--prefix=$INSTALL/$library "
-
-mkdir -p $BASE $SOURCE $BUILD $LOG
-
-case $library in
-    libxml2) 
-            LIB=libxml2                                     ;
-            REPO=git://git.gnome.org/libxml2                ;
-            if [[ -z "$LIBXML2_BRANCH" ]]; then
-                BRANCH=v2.9.4                               ;
-            else
-                BRANCH=$LIBXML2_BRANCH                      ;
-            fi                                              ;
-            CONFIG_FLAGS+=" --disable-shared  --without-zlib  --without-python  --without-readline  --without-threads  --without-history  --without-reader  --without-writer  --with-output  --without-ftp  --without-http  --without-pattern  --without-catalog  --without-docbook  --without-iconv  --without-schemas  --without-schematron  --without-modules  --without-xptr  --without-xinclude"                                         ;;
-    hdf5)   LIB=hdf5                                        ;
-            REPO=foo    ;
-            CONFIG_FLAGS+=" --enable-parallel"              ;;
-    fftw)   LIB=fftw                                        ;
-            REPO=git@github.com:FFTW/fftw3.git              ;
-            if [[ -z "$FFTW_BRANCH" ]]; then
-                BRANCH=$DEFAULT_BRANCH                      ;
-            else
-                BRANCH=$FFTW_BRANCH                         ;
-            fi                                              ;
-            CONFIG_FLAGS+=" --enable-maintainer-mode --enable-fma --enable-omp --enable-threads"
-            ;;
-    qmp)    LIB=qmp                                         ;
-            REPO=git@github.com:usqcd-software/qmp.git      ;
-            if [[ -z "$QMP_BRANCH" ]]; then
-                BRANCH=$DEFAULT_BRANCH                      ;
-            else
-                BRANCH=$QMP_BRANCH                          ;
-            fi                                              ;
-            CONFIG_FLAGS+="--with-qmp-comms-type=MPI --enable-openmp $CONFIG_QMP" ;;
-    qdpxx)  LIB=qdpxx                                       ;
-            REPO=git@github.com:azrael417/qdpxx.git         ;
-            if [[ -z "$QDPXX_BRANCH" ]]; then
-                BRANCH=$DEFAULT_BRANCH
-            else
-                BRANCH=$QDPXX_BRANCH                        ;
-            fi                                              ;
-            CONFIG_FLAGS+="--with-qmp=$INSTALL/qmp  --with-libxml2=$INSTALL/libxml2 --with-hdf5=${HDF5} --enable-openmp --enable-precision=double --enable-largefile --enable-parallel-io --enable-db-lite --enable-parallel-arch=parscalar $CONFIG_QDPXX" ;;
-    qphix)  echo "QPhiX not yet implemented."; 
-            echo "It requires icc compilers.";exit          ;
-            LIB=qphix                                       ;
-            REPO=git@github.com:JeffersonLab/qphix.git      ; 
-            CONFIG_FLAGS+="--with-qmp=$INSTALL/qmp --with-qdp++=$INSTALL/qdpxx --enable-parallel-arch=parscalar --enable-clover --enable-soalen=4 --disable-mm-malloc $CONFIG_QPHIX" ;;
-    quda)   echo "QUDA not yet implemented"; exit           ;
-            LIB=quda                                        ;
-            REPO=git@github.com:lattice/quda.git            ; 
-            CONFIG_FLAGS+="--with-qmp=$INSTALL/qmp --with-qdp=$INSTALL/qdpxx --with-cuda=/Developer/NVIDIA/CUDA-5.5 --enable-wilson-dirac --enable-clover-dirac --enable-staggered-dirac --enable-twisted-mass-dirac --enable-domain-wall-dirac --enable-numa-affinity --enable-hisq-force --enable-hisq-fatlink --enable-gauge-force --enable-milc-interface $CONFIG_QUDA" ;;
-    chroma) LIB=chroma                                      ;
-            REPO=git@github.com:JeffersonLab/chroma.git     ; 
-            if [[ -z "$CHROMA_BRANCH" ]]; then
-                BRANCH=$DEFAULT_BRANCH
-            else
-                BRANCH=$CHROMA_BRANCH                       ;
-            fi                                              ;
-            CONFIG_FLAGS+="--with-qmp=$INSTALL/qmp --with-qdp=$INSTALL/qdpxx --enable-openmp --enable-cpp-wilson-dslash $CONFIG_CHROMA" ;
-            if [[ "$GPUS" ]]; then
-                CONFIG_FLAGS+=" --with-quda=$INSTALL/quda ";
-            fi
-    all)    for lib in qmp libxml2 qdpxx chroma; do
-                echo "$0 $settings $lib $action"
-                $0 $settings $lib $action
-                status=$?
-                if [[ $status -ne 0 ]]; then
-                    echo ... failed; exit 1;
+case $ACTION in
+    get)
+        if [[ -d "$(UNQUOTE ${SOURCE[$LIBRARY]})" ]]; then
+            echo "Directory exists: $(UNQUOTE ${SOURCE[$LIBRARY]})"
+            overwrite=$(PROMPT_USER "Overwrite?" "NO")
+            if [[ "$overwrite" =~ ^[yY]*$ ]]; then
+                if [[ "/" != "$(UNQUOTE ${SOURCE[$LIBRARY]})" ]]; then
+                    rm -rf "$(UNQUOTE ${SOURCE[$LIBRARY]})"
+                else
+                    echo "Come on now... you don't really want to rm -rf /, right?"
+                    exit -1;
                 fi
-            done ;
-			exit ;;
-    *)      echo "Unknown library $library"; exit 1;;
-esac
-
-SOURCE=$SOURCE/$LIB
-BUILD=$BUILD/$LIB
-LOG=$LOG/$LIB
-INSTALL=$INSTALL/$LIB
-
-mkdir -p $LOG
-
-case $action in
-    fetch)
-        if [[ -d $SOURCE ]]; then
-            rm -rf $SOURCE;
+            else
+                echo "OK, not overwriting existing source directory."
+                exit;
+            fi
         fi
         echo "###"
-        echo "### FETCHING $LIB"
+        echo "### GETTING $LIBRARY"
         echo "###"
-        git clone --recursive $REPO $SOURCE;
-        pushd $SOURCE;
-            echo "CHECKING OUT BRANCH $BRANCH"
-            git checkout $BRANCH
-            echo "Updating submodules.";
-            git submodule update --init --recursive;
-            for olib in other_libs/*/ other_libs/*/other_libs/*/; do
-                if [[ ! -d "$olib" ]]; then continue; fi;
-                echo "Autotools: $olib"
-                (cd $olib; git checkout master; git pull; git pull;) > $LOG/autotools.$(basename $olib).log 2>&1;
-                (cd $olib; libtoolize; autoheader; aclocal; automake --add-missing; automake; autoconf; autoreconf -f;) >>  $LOG/autotools.$(basename $olib).log 2>&1
-            done;
-            echo "Autotools: $LIB";
-            (touch ChangeLog; libtoolize; autoheader ; aclocal ; automake --add-missing; automake --add-missing; autoconf ; autoreconf -f; libtoolize; autoheader ; aclocal ; automake --add-missing; automake --add-missing; autoconf ; autoreconf -f;) > $LOG/autotools.$LIB.log 2>&1
+        # echo "        tail -f $(UNQUOTE ${LOG[$LIBRARY]})/get.log"
+        # echo "$(UNQUOTE ${GET[$LIBRARY]})"
+        eval "$(UNQUOTE ${GET[$LIBRARY]})" # > $(UNQUOTE ${LOG[$LIBRARY]})/get.log
+        ;;
+    configure)
+        echo "###"
+        echo "### CONFIGURING $LIBRARY"
+        echo "###"
+        
+        if [[ "quda" == "$LIBRARY" ]]; then
+            echo "QUDA is annoying"; return;
+        fi
+        
+        for olib in $(UNQUOTE ${OTHER_LIBS[$LIBRARY]}); do
+            if [[ ! -d "$(UNQUOTE ${SOURCE[$LIBRARY]}/$olib)" ]]; then continue; fi
+            echo "Autotools: $olib"
+            # echo "        tail -f $(UNQUOTE ${LOG[$LIBRARY]})/autotools.$olib.log"
+            # echo "$(UNQUOTE ${SOURCE[$LIBRARY]}/$olib)"
+            (pushd $(UNQUOTE ${SOURCE[$LIBRARY]}/$olib); eval "$AUTOTOOLS"; popd) #> "$(UNQUOTE ${LOG[$LIBRARY]})/autotools.$olib.log";
+        done
+        
+        echo "Autotools: $LIBRARY";
+        # echo "        tail -f $(UNQUOTE ${LOG[$LIBRARY]})/autotools.$LIBRARY.log"
+        (
+        pushd $(UNQUOTE ${SOURCE[$LIBRARY]})
+        eval "$AUTOTOOLS" #> "$(UNQUOTE ${LOG[$LIBRARY]})/autotools.$LIBRARY.log"
+        popd
+        ) 
+        
+        pushd $(UNQUOTE "${BUILD[$LIBRARY]}")
+
+        echo "Configuring $LIB"
+
+        echo "================================================"
+        
+        echo "CC        $CC"
+        echo "          $($CC --version | head -n 1)"
+        echo "CXX       $CXX"
+        echo "          $($CXX --version | head -n 1)"
+        echo "CFLAGS    $(UNQUOTE ${C_FLAGS[$LIBRARY]})"
+        echo "CXXFLAGS  $(UNQUOTE ${CXX_FLAGS[$LIBRARY]})"
+        echo "HOST      $HOST"
+        echo "FFTW      $(UNQUOTE ${INSTALL[fftw]})"    #TODO: replace with a loop over the stack.
+        echo "HDF5      $(UNQUOTE ${INSTALL[hdf5]})"    #TODO: replace with a loop over the stack.
+        
+        echo "================================================"
+        
+        echo "CC=\"$CC\" CFLAGS=\"$(UNQUOTE "${C_FLAGS[$LIBRARY]}")\" CXX=\"$CXX\" CXXFLAGS=\"$(UNQUOTE "${CXX_FLAGS[$LIBRARY]}")\" $(UNQUOTE "${CONFIGURE[$LIBRARY]} ${CONFIG_FLAGS[$LIBRARY]}")"
+        echo "        tail -f $(UNQUOTE ${LOG[$LIBRARY]})/configure.log"
+        eval "CC=\"$CC\" CFLAGS=\"$(UNQUOTE "${C_FLAGS[$LIBRARY]}")\" CXX=\"$CXX\" CXXFLAGS=\"$(UNQUOTE "${CXX_FLAGS[$LIBRARY]}")\" $(UNQUOTE "${CONFIGURE[$LIBRARY]} ${CONFIG_FLAGS[$LIBRARY]}")" > "$(UNQUOTE ${LOG[$LIBRARY]})/configure.log"
+        if [[ $status -ne 0 ]]; then
+            echo ... failed; exit 1;
+        fi
         popd
         ;;
     make) 
-        if [[ -d $BUILD ]]; then
-            rm -rf $BUILD;
+        echo "###"
+        echo "### COMPILING $LIBRARY"
+        echo "###"
+        echo "        tail -f $(UNQUOTE ${LOG[$LIBRARY]})/make.log"
+        
+        pushd $(UNQUOTE "${BUILD[$LIBRARY]}")
+        MAKE_LOG="$(UNQUOTE ${LOG[$LIBRARY]})/make.log"
+        $MAKE > $MAKE_LOG 2>&1
+        if [[ $status -ne 0 ]]; then
+            echo ... failed; exit 1;
         fi
-        echo "###"
-        echo "### MAKING $LIB"
-        echo "###"
-        mkdir -p $BUILD;
-        pushd $BUILD;
-            echo "Configuring $LIB"
-
-            echo "================================================"
-            
-            echo "CC        $CC"
-            echo "          $($CC --version | head -n 1)"
-            echo "CXX       $CXX"
-            echo "          $($CXX --version | head -n 1)"
-            echo "CFLAGS    $CFLAGS"
-            echo "CXXFLAGS  $CXXFLAGS"
-            echo "HOST      $HOST"
-            echo "FFTW      $FFTW"
-            echo "HDF5      $HDF5"
-            
-            echo "================================================"
-            
-            echo $SOURCE/configure $CONFIG_FLAGS CC=\"$CC\" CFLAGS=\"$CFLAGS\" CXX=\"$CXX\" CXXFLAGS=\"$CXXFLAGS\" --host=$HOST --build=${BUILD_FLAG}
-            $SOURCE/configure $CONFIG_FLAGS CC="$CC" CFLAGS="$CFLAGS" CXX="$CXX" CXXFLAGS="$CXXFLAGS"  --host=$HOST --build=${BUILD_FLAG} > $LOG/configure.log 2>&1
-            status=$?
-            if [[ $status -ne 0 ]]; then
-                echo ... failed; exit 1;
-            fi
-            echo "Making $LIB"
-            make -j $MAKE_JOBS > $LOG/make.log 2>&1
-            status=$?
-            if [[ $status -ne 0 ]]; then
-                echo ... failed; exit 1;
-            fi
-        popd;
+        popd
         ;;
     install)
-        if [[ ! -d $BUILD ]]; then
+        if [[ ! -d $(UNQUOTE "${BUILD[$LIBRARY]}") ]]; then
             echo "Build first."
-            exit 1
+            exit
         fi
         echo "###"
-        echo "### INSTALLING $LIB"
+        echo "### INSTALLING $LIBRARY"
         echo "###"
-        pushd $BUILD;
-            mkdir -p $INSTALL;
-            make install > $LOG/install.log 2>&1;
-            status=$?
-            if [[ $status -ne 0 ]]; then
-                echo ... failed; exit 1;
-            fi
-        popd;
-        ;;
-    clean)
-        rm -rf $SOURCE $BUILD $INSTALL $LOG 2>/dev/null
+        echo "        tail -f $(UNQUOTE ${LOG[$LIBRARY]})/install.log"
+        pushd $(UNQUOTE "${BUILD[$LIBRARY]}")
+        $MAKE install > "$(UNQUOTE ${LOG[$LIBRARY]})/install.log" 2>&1
+        if [[ $status -ne 0 ]]; then
+            echo ... failed; exit 1;
+        fi
+        popd
         ;;
     complete)
-        for action in fetch make install; do
-            echo "$0 $settings $LIB $action"
-            $0 $settings $LIB $action
+        for action in get configure make install; do
+            #echo "$this_script $settings $LIB $action"
+            $this_script $MACHINE $LIBRARY $action
             status=$?
             if [[ $status -ne 0 ]]; then
                 echo ... failed; exit 1;
             fi
         done ;
-		exit ;;
-    *)      echo "Unknown action $action"; exit 1;;
+        exit ;;
+    *)
+    echo "Unknown action $ACTION"; exit 1;;
 esac

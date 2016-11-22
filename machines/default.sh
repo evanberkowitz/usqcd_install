bb5fc2af510696ed50bc72ac70f8d19fbca0fb66
Evan Berkowitz <git@evanberkowitz.com>
2016-11-22 22:41:52 +0100

    _single targets

--- machines/default.sh
+++ machines/default.sh
@@ -1,4 +1,4 @@
-LIBS=("qmp" "libxml2" "hdf5" "fftw" "qdpxx" "quda" "qphix" "chroma")
+LIBS=("qmp" "libxml2" "hdf5" "fftw" "qdpxx" "qdpxx_single" "quda" "qphix" "chroma" "chroma_single")
 
 unset DIR
 declare -A DIR
@@ -20,9 +20,11 @@ GET[libxml2]='curl ftp://xmlsoft.org/libxml2/libxml2-2.9.4.tar.gz -o ${SOURCE[$L
 GET[hdf5]='curl https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.17.tar -o ${SOURCE[$LIBRARY]%/*}/hdf5-1.8.17.tar; pushd; tar -xzf ${SOURCE[$LIBRARY]%/*}/hdf5-1.8.17.tar; popd;'
 GET[fftw]='${GIT_CLONE} git@github.com:FFTW/fftw3.git ${SOURCE[$LIBRARY]}; '
 GET[qdpxx]='${GIT_CLONE} git@github.com:azrael417/qdpxx.git ${SOURCE[$LIBRARY]}; pushd ${SOURCE[$LIBRARY]}; git checkout ${GIT_BRANCH[qdpxx]}; ${GIT_UPDATE_SUBMODULES}; '
+GET[qdpxx_single]='${GIT_CLONE} git@github.com:azrael417/qdpxx.git ${SOURCE[$LIBRARY]}; pushd ${SOURCE[$LIBRARY]}; git checkout ${GIT_BRANCH[qdpxx]}; ${GIT_UPDATE_SUBMODULES}; '
 GET[quda]='${GIT_CLONE} git@github.com:lattice/quda.git ${SOURCE[$LIBRARY]}; pushd ${SOURCE[$LIBRARY]}; git checkout ${GIT_BRANCH[quda]}; ${GIT_UPDATE_SUBMODULES}; '
 GET[qphix]='${GIT_CLONE} git@github.com:JeffersonLab/qphix.git ${SOURCE[$LIBRARY]}; pushd ${SOURCE[$LIBRARY]}; git checkout ${GIT_BRANCH[qphix]}; ${GIT_UPDATE_SUBMODULES}; '
 GET[chroma]='${GIT_CLONE} git@github.com:JeffersonLab/chroma.git ${SOURCE[$LIBRARY]}; pushd ${SOURCE[$LIBRARY]}; git checkout devel; ${GIT_UPDATE_SUBMODULES}; '
+GET[chroma_single]='${GIT_CLONE} git@github.com:JeffersonLab/chroma.git ${SOURCE[$LIBRARY]}; pushd ${SOURCE[$LIBRARY]}; git checkout devel; ${GIT_UPDATE_SUBMODULES}; '
 
 unset GIT_BRANCH
 declare -A GIT_BRANCH
@@ -32,9 +34,11 @@ GIT_BRANCH[libxml2]="v2.9.4"
 GIT_BRANCH[hdf5]=""
 GIT_BRANCH[fftw]="master"
 GIT_BRANCH[qdpxx]="devel"
+GIT_BRANCH[qdpxx_single]="devel"
 GIT_BRANCH[quda]="devel"
 GIT_BRANCH[qphix]="devel"
 GIT_BRANCH[chroma]="devel"
+GIT_BRANCH[chroma_single]="devel"
 
 
 unset SOURCE
@@ -45,9 +49,11 @@ SOURCE[libxml2]='${DIR[SOURCE]}/libxml2'
 SOURCE[hdf5]='${DIR[SOURCE]}/hdf5'
 SOURCE[fftw]='${DIR[SOURCE]}/fftw'
 SOURCE[qdpxx]='${DIR[SOURCE]}/qdpxx'
+SOURCE[qdpxx_single]='${DIR[SOURCE]}/qdpxx_single'
 SOURCE[quda]='${DIR[SOURCE]}/quda'
 SOURCE[qphix]='${DIR[SOURCE]}/qphix'
 SOURCE[chroma]='${DIR[SOURCE]}/chroma'
+SOURCE[chroma_single]='${DIR[SOURCE]}/chroma_single'
 
 unset BUILD
 declare -A BUILD
@@ -57,9 +63,11 @@ BUILD[libxml2]='${DIR[BUILD]}/libxml2'
 BUILD[hdf5]='${DIR[BUILD]}/hdf5'
 BUILD[fftw]='${DIR[BUILD]}/fftw'
 BUILD[qdpxx]='${DIR[BUILD]}/qdpxx'
+BUILD[qdpxx_single]='${DIR[BUILD]}/qdpxx_single'
 BUILD[quda]='${DIR[INSTALL]}/quda'  # I know this looks wrong, but the current cmake build system just wants you to build in the install directory.
 BUILD[qphix]='${DIR[BUILD]}/qphix'
 BUILD[chroma]='${DIR[BUILD]}/chroma'
+BUILD[chroma_single]='${DIR[BUILD]}/chroma_single'
 
 unset LOG
 declare -A LOG
@@ -69,9 +77,11 @@ LOG[libxml2]='${DIR[LOG]}/libxml2'
 LOG[hdf5]='${DIR[LOG]}/hdf5'
 LOG[fftw]='${DIR[LOG]}/fftw'
 LOG[qdpxx]='${DIR[LOG]}/qdpxx'
+LOG[qdpxx_single]='${DIR[LOG]}/qdpxx_single'
 LOG[quda]='${DIR[LOG]}/quda'
 LOG[qphix]='${DIR[LOG]}/qphix'
 LOG[chroma]='${DIR[LOG]}/chroma'
+LOG[chroma_single]='${DIR[LOG]}/chroma_single'
 
 unset INSTALL
 declare -A INSTALL
@@ -81,9 +91,11 @@ INSTALL[libxml2]='${DIR[INSTALL]}/libxml2'
 INSTALL[hdf5]='${DIR[INSTALL]}/hdf5'
 INSTALL[fftw]='${DIR[INSTALL]}/fftw'
 INSTALL[qdpxx]='${DIR[INSTALL]}/qdpxx'
+INSTALL[qdpxx]='${DIR[INSTALL]}/qdpxx_single'
 INSTALL[quda]='${DIR[INSTALL]}/quda'
 INSTALL[qphix]='${DIR[INSTALL]}/qphix'
 INSTALL[chroma]='${DIR[INSTALL]}/chroma'
+INSTALL[chroma_single]='${DIR[INSTALL]}/chroma_single'
 
 unset OTHER_LIBS
 declare -A OTHER_LIBS
@@ -93,9 +105,11 @@ OTHER_LIBS[libxml2]=""
 OTHER_LIBS[hdf5]=""
 OTHER_LIBS[fftw]=""
 OTHER_LIBS[qdpxx]="other_libs/filedb other_libs/libintrin other_libs/qio/other_libs/c-lime other_libs/qio other_libs/xpath_reader "
+OTHER_LIBS[qdpxx_single]="other_libs/filedb other_libs/libintrin other_libs/qio/other_libs/c-lime other_libs/qio other_libs/xpath_reader "
 OTHER_LIBS[quda]=""
 OTHER_LIBS[qphix]=""
 OTHER_LIBS[chroma]="other_libs/cg-dwf other_libs/cpp_wilson_dslash other_libs/qdp-lapack other_libs/sse_wilson_dslash other_libs/wilsonmg"
+OTHER_LIBS[chroma_single]="other_libs/cg-dwf other_libs/cpp_wilson_dslash other_libs/qdp-lapack other_libs/sse_wilson_dslash other_libs/wilsonmg"
 
 unset LIBS
 declare -A LIBS
@@ -105,9 +119,11 @@ LIBS[libxml2]='${LIBS[DEFAULT]}'
 LIBS[hdf5]='${LIBS[DEFAULT]}'
 LIBS[fftw]='${LIBS[DEFAULT]}'
 LIBS[qdpxx]='${LIBS[DEFAULT]}'
+LIBS[qdpxx_single]='${LIBS[DEFAULT]}'
 LIBS[quda]='${LIBS[DEFAULT]}'
 LIBS[qphix]='${LIBS[DEFAULT]}'
 LIBS[chroma]='${LIBS[DEFAULT]}'
+LIBS[chroma_single]='${LIBS[DEFAULT]}'
 
 
 unset CONFIGURE
@@ -118,9 +134,11 @@ CONFIGURE[libxml2]='${SOURCE[libxml2]}/configure '
 CONFIGURE[hdf5]='${SOURCE[hdf5]}/configure '
 CONFIGURE[fftw]='${SOURCE[fftw]}/configure '
 CONFIGURE[qdpxx]='${SOURCE[qdpxx]}/configure '
+CONFIGURE[qdpxx_single]='${SOURCE[qdpxx_single]}/configure '
 CONFIGURE[quda]='cmake ${SOURCE[quda]} '
 CONFIGURE[qphix]='${SOURCE[qphix]}/configure'
 CONFIGURE[chroma]='${SOURCE[chroma]}/configure '
+CONFIGURE[chroma]='${SOURCE[chroma_single]}/configure '
 
 unset CONFIG_FLAGS
 declare -A CONFIG_FLAGS
@@ -130,9 +148,11 @@ CONFIG_FLAGS[libxml2]='--prefix=${INSTALL[libxml2]}  --disable-shared  --without
 CONFIG_FLAGS[hdf5]='--prefix=${INSTALL[hdf5]} '
 CONFIG_FLAGS[fftw]='--prefix=${INSTALL[fftw]} '
 CONFIG_FLAGS[qdpxx]='--prefix=${INSTALL[qdpxx]} --with-qmp=${INSTALL[qmp]} --with-libxml2=${INSTALL[libxml2]} --with-hdf5=${INSTALL[hdf5]} --enable-openmp --enable-precision=double --enable-largefile --enable-parallel-io --enable-db-lite --enable-parallel-arch=parscalar'
+CONFIG_FLAGS[qdpxx_single]='--prefix=${INSTALL[qdpxx_single]} --with-qmp=${INSTALL[qmp]} --with-libxml2=${INSTALL[libxml2]} --with-hdf5=${INSTALL[hdf5]} --enable-openmp --enable-precision=single --enable-largefile --enable-parallel-io --enable-db-lite --enable-parallel-arch=parscalar'
 CONFIG_FLAGS[quda]='-DQUDA_GPU_ARCH=${GPU_ARCH} -DQUDA_MPI=ON -DQUDA_QMP=ON -DQUDA_QMPHOME=${INSTALL[qmp]} -DMPI_C_COMPILER=${CC} -DMPI_CXX_COMPILER=${CXX} -DQUDA_DIRAC_WILSON=ON -DQUDA_DIRAC_DOMAIN_WALL=ON ' # -DQUDA_DIRAC_TWISTED_MASS=OFF  -DQUDA_LINK_HISQ=OFF -DQUDA_FORCE_GAUGE=OFF -DQUDA_FORCE_HISQ=OFF
 CONFIG_FLAGS[qphix]='--prefix=${INSTALL[qphix]}   --with-qmp=${INSTALL[qmp]} --enable-parallel-arch=parscalar --disable-mm-malloc ' # --with-qdp and --with-qdp++ seem to fail.
 CONFIG_FLAGS[chroma]='--prefix=${INSTALL[chroma]} --with-qmp=${INSTALL[qmp]} --with-qdp=${INSTALL[qdpxx]} --enable-openmp '
+CONFIG_FLAGS[chroma_single]='--prefix=${INSTALL[chroma_single]} --with-qmp=${INSTALL[qmp]} --with-qdp=${INSTALL[qdpxx_single]} --enable-openmp '
 
 unset CXX_FLAGS
 declare -A CXX_FLAGS
@@ -142,9 +162,11 @@ CXX_FLAGS[libxml2]='${CXX_FLAGS[DEFAULT]}'
 CXX_FLAGS[hdf5]='${CXX_FLAGS[DEFAULT]}'
 CXX_FLAGS[fftw]='${CXX_FLAGS[DEFAULT]}'
 CXX_FLAGS[qdpxx]='${CXX_FLAGS[DEFAULT]}'
+CXX_FLAGS[qdpxx_single]='${CXX_FLAGS[DEFAULT]}'
 CXX_FLAGS[quda]='${CXX_FLAGS[DEFAULT]}'
 CXX_FLAGS[qphix]='${CXX_FLAGS[DEFAULT]}'
 CXX_FLAGS[chroma]='${CXX_FLAGS[DEFAULT]}'
+CXX_FLAGS[chroma_single]='${CXX_FLAGS[DEFAULT]}'
 
 unset C_FLAGS
 declare -A C_FLAGS
@@ -154,9 +176,11 @@ C_FLAGS[libxml2]='${C_FLAGS[DEFAULT]}'
 C_FLAGS[hdf5]='${C_FLAGS[DEFAULT]}'
 C_FLAGS[fftw]='${C_FLAGS[DEFAULT]}'
 C_FLAGS[qdpxx]='${C_FLAGS[DEFAULT]}'
+C_FLAGS[qdpxx_single]='${C_FLAGS[DEFAULT]}'
 C_FLAGS[quda]='${C_FLAGS[DEFAULT]}'
 C_FLAGS[qphix]='${C_FLAGS[DEFAULT]}'
 C_FLAGS[chroma]='${C_FLAGS[DEFAULT]}'
+C_FLAGS[chroma_single]='${C_FLAGS[DEFAULT]}'
 
 unset QUDA_LIBS
 declare -A QUDA_LIBS
@@ -169,9 +193,11 @@ QUDA_LIBS[libxml2]='${QUDA_LIBS[DEFAULT]}'
 QUDA_LIBS[hdf5]='${QUDA_LIBS[DEFAULT]}'
 QUDA_LIBS[fftw]='${QUDA_LIBS[DEFAULT]}'
 QUDA_LIBS[qdpxx]='${QUDA_LIBS[DEFAULT]}'
+QUDA_LIBS[qdpxx_single]='${QUDA_LIBS[DEFAULT]}'
 QUDA_LIBS[quda]='${QUDA_LIBS[DEFAULT]}'
 QUDA_LIBS[qphix]='${QUDA_LIBS[DEFAULT]}'
 QUDA_LIBS[chroma]='${QUDA_LIBS[DEFAULT]}'
+QUDA_LIBS[chroma_single]='${QUDA_LIBS[DEFAULT]}'
 
 unset LD_FLAGS
 declare -A LD_FLAGS
@@ -181,10 +207,11 @@ LD_FLAGS[libxml2]='${LD_FLAGS[DEFAULT]}'
 LD_FLAGS[hdf5]='${LD_FLAGS[DEFAULT]}'
 LD_FLAGS[fftw]='${LD_FLAGS[DEFAULT]}'
 LD_FLAGS[qdpxx]='${LD_FLAGS[DEFAULT]}'
+LD_FLAGS[qdpxx_single]='${LD_FLAGS[DEFAULT]}'
 LD_FLAGS[quda]='${LD_FLAGS[DEFAULT]}'
 LD_FLAGS[qphix]='${LD_FLAGS[DEFAULT]}'
 LD_FLAGS[chroma]='${LD_FLAGS[DEFAULT]} -Wl,-zmuldefs'
-
+LD_FLAGS[chroma_single]='${LD_FLAGS[DEFAULT]} -Wl,-zmuldefs'
 
 
 

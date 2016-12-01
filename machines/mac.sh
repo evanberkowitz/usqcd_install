9527f95c7d3468a9d6498ccd6577933204b373b1
Evan Berkowitz <git@evanberkowitz.com>
2016-12-01 14:24:29 +0100

    Update mac install
    Oh man, this was tricky.  I kept getting errors that said my compiler couldn't produce executables.  But only for chroma.
    
    I tried reinstalling open-mpi with almost every combination available on homebrew until I found the `--without-fortran` flag to be the key.
    
    But, even with that reinstalled, I still didn't succeed in compiling chroma.  The issue turned out to be the updated `LD_FLAGS[chroma]` defaults, which included `-Wl,zmuldefs`.  Getting rid of that on the mac cured the issue.

--- machines/mac.sh
+++ machines/mac.sh
@@ -1,22 +1,27 @@
 BASE=$HOME/USQCD
-STACK="qmp qdpxx qdpxx_single chroma chroma_single"
+STACK="qmp qdpxx chroma qdpxx_single chroma_single"
 
 
 # brew install gcc
 export HOMEBREW_CC=gcc-6
 export HOMEBREW_CXX=g++-6
-#GET[libxml2]="brew install libxml2"
-#GET[fftw]="brew install fftw --with-mpi --with-openmp"
-#GET[hdf5]="brew install hdf5 --with-mpi --c++11"
+# brew install open-mpi  --without-fortran --with-cxx-bindings --c++11 --with-mpi-thread-multiple  # --without-fortran is key to avoid `make check` errors on my system.
+GET[libxml2]="brew install libxml2"
+GET[fftw]="brew install fftw --with-mpi --with-openmp"
+GET[hdf5]="brew install hdf5 --with-mpi --c++11"
 INSTALL[libxml2]=/usr/local/opt/libxml2
 INSTALL[hdf5]=/usr/local
 INSTALL[fftw]=/usr/local
 
 
-CC=/usr/local/bin/mpicc
+export CC=/usr/local/bin/mpicc
 C_FLAGS[DEFAULT]="-O3 -march=corei7 -fpermissive -fopenmp"
 
-CXX=/usr/local/bin/mpicxx
+export CXX=/usr/local/bin/mpicxx
 CXX_FLAGS[DEFAULT]="-O3 -std=c++11 -march=corei7 -ffast-math -funroll-all-loops -fpermissive -fopenmp"
+
+# No zmuldefs.
+LD_FLAGS[chroma]='${LD_FLAGS[DEFAULT]}'
+LD_FLAGS[chroma_single]='${LD_FLAGS[DEFAULT]}'
 
 LIBTOOLIZE=glibtoolize

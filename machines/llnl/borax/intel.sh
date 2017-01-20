94bfe264bd0e03c359e07097e70aa3c862dcf434
Evan Berkowitz <berkowitz2@llnl.gov>
2017-01-20 05:18:51 -0800

    aztec --> borax

--- machines/llnl/borax/intel.sh
+++ machines/llnl/borax/intel.sh
@@ -1,20 +1,17 @@
-BASE=/nfs/tmp2/lattice_qcd/aztec/intel/USQCD
-
-# load dotkit
-. /usr/local/tools/dotkit/init.sh
+BASE=/nfs/tmp2/lattice_qcd/borax/intel/USQCD
 
 #module purge
-module unload hdf5
-use hdf5-intel-parallel-mvapich2-1.8.16    > /dev/null
-use mvapich2-intel-2.2                     > /dev/null
-module load intel/16.0
+module load intel/16.0.3
+module load hdf5-parallel/1.8.17
+module load mvapich2/2.2
+
 #module load fftw/3.2
 
 HOST=x86_64-linux-gnu
 
 
 GET[libxml2]='curl ftp://xmlsoft.org/libxml2/libxml2-2.9.4.tar.gz -o ${SOURCE[$LIBRARY]%/*}/libxml2-2.9.4.tar.gz; pushd ${SOURCE[$LIBRARY]%/*}; tar -xzf libxml2-2.9.4.tar.gz; mv libxml2-2.9.4 ${SOURCE[$LIBRARY]}; popd;'
-INSTALL[hdf5]=$HDF5
+INSTALL[hdf5]=/usr/tce/packages/hdf5/hdf5-parallel-1.8.17-intel-16.0.3-mvapich2-2.2
 INSTALL[fftw]=${FFTW_LIBS%/*}
 
 STACK="qmp libxml2 qdpxx chroma"

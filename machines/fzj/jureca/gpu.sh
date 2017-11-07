4307dacc3d80ec2ce76a8c41341a7d851a7c88b3
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2017-11-07 10:37:38 +0100

    Update JURECA CPU to new software stage

--- machines/fzj/jureca/gpu.sh
+++ machines/fzj/jureca/gpu.sh
@@ -5,10 +5,10 @@ GPU_ARCH=sm_35  # Kepler GK110 at Keeneland
 GPUS=1
 
 module purge
-module load GCC/5.4.0 ParaStationMPI/5.1.9-1 CUDA/8.0.61 CMake/3.7.2 2> /dev/null #HDF5/1.8.18 FFTW/3.3.6
+module load GCC/7.2.0 ParaStationMPI/5.2.0-1 CUDA/8.0.61 CMake/3.7.2 2> /dev/null
 
-INSTALL[hdf5]=/usr/local/software/jureca/Stages/2017a/software/HDF5/1.8.18-gpsmpi-2017a
-INSTALL[fftw]=/usr/local/software/jureca/Stages/2016b/software/FFTW/3.3.6-gpsmpi-2017b/
+INSTALL[hdf5]=/usr/local/software/jureca/Stages/2017a/software/HDF5/1.8.19-gpsmpi-2017b
+INSTALL[fftw]=/usr/local/software/jureca/Stages/2016b/software/FFTW/3.3.6-gpsmpi-2017b
 
 GET[libxml2]='curl ftp://xmlsoft.org/libxml2/libxml2-2.9.4.tar.gz -o ${SOURCE[$LIBRARY]%/*}/libxml2-2.9.4.tar.gz; pushd ${SOURCE[$LIBRARY]%/*}; tar -xzf libxml2-2.9.4.tar.gz; mv libxml2-2.9.4 ${SOURCE[$LIBRARY]}; popd;'
 

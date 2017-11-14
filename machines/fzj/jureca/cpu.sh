649857f68ced6e35e99af68369de90a71bab2c1a
Jan-Lukas Wynen <jl-wynen@users.noreply.github.com>
2017-11-14 10:55:55 +0100

    Update JURECA CPU to new software stage (#6)
    Update FZJ modules etc.

--- machines/fzj/jureca/cpu.sh
+++ machines/fzj/jureca/cpu.sh
@@ -1,10 +1,12 @@
-BASE=$HOME/USQCD/jureca
+BASE=/homea/jias41/jias4102/USQCD/jureca
 
 HOST=x86_64-linux-gnu
 
-module load GCC/5.4.0 ParaStationMPI/5.1.9-1 HDF5/1.8.18 FFTW/3.3.6
-INSTALL[hdf5]=/usr/local/software/jureca/Stages/2017a/software/HDF5/1.8.18-gpsmpi-2017a
-INSTALL[fftw]=/usr/local/software/jureca/Stages/2016b/software/FFTW/3.3.6-gpsmpi-2017b/
+module purge
+module load GCC/7.2.0 ParaStationMPI/5.2.0-1 HDF5/1.8.19 FFTW/3.3.6
+
+INSTALL[hdf5]=/usr/local/software/jureca/Stages/2017a/software/HDF5/1.8.19-gpsmpi-2017b
+INSTALL[fftw]=/usr/local/software/jureca/Stages/2016b/software/FFTW/3.3.6-gpsmpi-2017b
 
 GET[libxml2]='curl ftp://xmlsoft.org/libxml2/libxml2-2.9.4.tar.gz -o ${SOURCE[$LIBRARY]%/*}/libxml2-2.9.4.tar.gz; pushd ${SOURCE[$LIBRARY]%/*}; tar -xzf libxml2-2.9.4.tar.gz; mv libxml2-2.9.4 ${SOURCE[$LIBRARY]}; popd;'
 
